7f65d02063fddb461eb43b6c15b773165b6be011
Ammar Mohamed Amin <153574317+AmmarMohamed0@users.noreply.github.com>
2024-12-21 04:37:24 +0200

    added new function and test case

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,7 +1,11 @@
-from src.main import add
+from src.main import add, subtract
 
 def test_add_functoin():
     assert add(2, 3) == 5
-    assert add(0, 0) == 1
+    assert add(0, 0) == 0
     assert add(5, 5) == 10
     
+def test_subtract_functoin():
+    assert subtract(5, 3) == 2
+    assert subtract(0, 0) == 0
+    assert subtract(10, 5) == 5
